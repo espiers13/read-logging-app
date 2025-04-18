e8a7f0301bd2ff553752aeaa8ce8ac18ef31e224
Emily <e.spiers13@gmail.com>
2025-04-18 09:24:49 +0100

    mobile only homepage created

--- dear-reader/src/App.jsx
+++ dear-reader/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import useIsMobile from "./custom-hooks/useIsMobile";
 import "./index.css";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
@@ -17,12 +18,14 @@ import Settings from "./pages/Settings";
 import Bookshelf from "./pages/Bookshelf";
 import Journal from "./pages/Journal";
 import FriendProfile from "./pages/FriendProfile";
+import MobileOnlyGate from "./components/MobileOnlyGate";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(() => {
     const savedUser = localStorage.getItem("currentUser");
     return savedUser ? JSON.parse(savedUser) : null;
   });
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     if (currentUser) {
@@ -34,90 +37,92 @@ function App() {
 
   return (
     <div className="app-container">
-      <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
-      <main className="flex-grow">
-        <Routes>
-          {/* Conditional route for homepage */}
-          <Route
-            path="/"
-            element={
-              currentUser ? (
-                <Homepage currentUser={currentUser} />
-              ) : (
-                <Navigate to={`/login`} />
-              )
-            }
-          />
+      <MobileOnlyGate>
+        <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
+        <main className="flex-grow">
+          <Routes>
+            {/* Conditional route for homepage */}
+            <Route
+              path="/"
+              element={
+                currentUser ? (
+                  <Homepage currentUser={currentUser} />
+                ) : (
+                  <Navigate to={`/login`} />
+                )
+              }
+            />
 
-          {/* Login and SignUp pages, redirected if currentUser exists */}
-          <Route
-            path="/login"
-            element={
-              currentUser ? (
-                <Navigate to={`/${currentUser.id}/profile`} />
-              ) : (
-                <Login setCurrentUser={setCurrentUser} />
-              )
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              currentUser ? (
-                <Navigate to={`/${currentUser.id}/profile`} />
-              ) : (
-                <SignUp setCurrentUser={setCurrentUser} />
-              )
-            }
-          />
+            {/* Login and SignUp pages, redirected if currentUser exists */}
+            <Route
+              path="/login"
+              element={
+                currentUser ? (
+                  <Navigate to={`/${currentUser.id}/profile`} />
+                ) : (
+                  <Login setCurrentUser={setCurrentUser} />
+                )
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                currentUser ? (
+                  <Navigate to={`/${currentUser.id}/profile`} />
+                ) : (
+                  <SignUp setCurrentUser={setCurrentUser} />
+                )
+              }
+            />
 
-          {/* Book, Search, Profile, etc. Routes */}
-          <Route
-            path="/book/:book_id"
-            element={<BookInfo currentUser={currentUser} />}
-          />
-          <Route path="/search" element={<Search />} />
-          <Route
-            path="/:user_id/friends"
-            element={<Friends currentUser={currentUser} />}
-          />
-          <Route
-            path="/:user_id/bookshelf"
-            element={<Bookshelf currentUser={currentUser} />}
-          />
-          <Route
-            path="/:user_id/journal"
-            element={<Journal currentUser={currentUser} />}
-          />
-          <Route
-            path="/:user_id/profile"
-            element={<Profile currentUser={currentUser} />}
-          />
-          <Route path="/search/:search_query" element={<SearchResults />} />
-          <Route path="/search/genre/:genre" element={<SearchByGenre />} />
-          <Route
-            path="/:user_id/settings"
-            element={
-              <Settings
-                currentUser={currentUser}
-                setCurrentUser={setCurrentUser}
-              />
-            }
-          />
-          <Route
-            path="/user/:friend_username/:friend_id"
-            element={<FriendProfile currentUser={currentUser} />}
-          />
+            {/* Book, Search, Profile, etc. Routes */}
+            <Route
+              path="/book/:book_id"
+              element={<BookInfo currentUser={currentUser} />}
+            />
+            <Route path="/search" element={<Search />} />
+            <Route
+              path="/:user_id/friends"
+              element={<Friends currentUser={currentUser} />}
+            />
+            <Route
+              path="/:user_id/bookshelf"
+              element={<Bookshelf currentUser={currentUser} />}
+            />
+            <Route
+              path="/:user_id/journal"
+              element={<Journal currentUser={currentUser} />}
+            />
+            <Route
+              path="/:user_id/profile"
+              element={<Profile currentUser={currentUser} />}
+            />
+            <Route path="/search/:search_query" element={<SearchResults />} />
+            <Route path="/search/genre/:genre" element={<SearchByGenre />} />
+            <Route
+              path="/:user_id/settings"
+              element={
+                <Settings
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+            <Route
+              path="/user/:friend_username/:friend_id"
+              element={<FriendProfile currentUser={currentUser} />}
+            />
 
-          {/* Page Not Found Route */}
-          <Route path="*" element={<Navigate to="/404" />} />
-          <Route
-            path="/404"
-            element={<PageNotFound currentUser={currentUser} />}
-          />
-        </Routes>
-      </main>
-      <Footer currentUser={currentUser} />
+            {/* Page Not Found Route */}
+            <Route path="*" element={<Navigate to="/404" />} />
+            <Route
+              path="/404"
+              element={<PageNotFound currentUser={currentUser} />}
+            />
+          </Routes>
+        </main>
+        <Footer currentUser={currentUser} />
+      </MobileOnlyGate>
     </div>
   );
 }
